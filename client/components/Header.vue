944190296d8ad9268d259da64b41a9373cc7f62c
akihiro iwata <akihiro.iwata.1990@gmail.com>
2018-11-26 15:50:45 +0900

    Headerを共通コンポーネント化

---
+++ client/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="header">
+    <img
+      src="../assets/img/logo.png"
+      style="width: auto; height: 50px">
+    <div style="width: 15px"/><!-- 隙間 -->
+    <span class="logo">Code Climbing</span>
+    <div style="width: 50vw"/><!-- 隙間 -->
+    <span class="nameLabel">名前</span>
+    <div style="width: 15px"/><!-- 隙間 -->
+    <input
+      class="nameBox"
+      type="text"
+      placeholder="名前を入力してください">
+  </div><!-- 終点: header -->
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.header {
+  height: 8vh;
+  width: 100vw;
+  padding-left: 1vw;
+  padding-top: 1vh;
+  padding-bottom: 1vh;
+  border-bottom: black 1px solid;
+  display: flex;
+  align-items: center;
+
+  .logo {
+    font-size: 28px;
+    font-family: adelle, serif;
+    font-style: normal;
+    font-weight: bold;
+  }
+
+  .nameLabel {
+    font-family: 'Noto Sans JP', sans-serif;
+  }
+
+  .nameBox {
+    font-family: 'Noto Sans JP', sans-serif;
+    width: 20vw;
+  }
+}
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,19 +1,6 @@
 <template>
   <div class="page">
-    <div class="header">
-      <img
-        src="../assets/img/logo.png"
-        style="width: auto; height: 50px">
-      <div style="width: 15px"/><!-- 隙間 -->
-      <span class="logo">Code Climbing</span>
-      <div style="width: 50vw"/><!-- 隙間 -->
-      <span class="nameLabel">名前</span>
-      <div style="width: 15px"/><!-- 隙間 -->
-      <input
-        class="nameBox"
-        type="text"
-        placeholder="名前を入力してください">
-    </div><!-- 終点: header -->
+    <Header/>
     <div style="height: 10px; width: 100vw"/><!-- 隙間 -->
     <div class="contents">
       <div class="question"><!-- 問題文 -->
@@ -45,9 +32,12 @@
 </template>
 
 <script>
+import Header from '../components/Header'
+
 export default {
   components: {
-    editor: require('vue2-ace-editor')
+    editor: require('vue2-ace-editor'),
+    Header: Header
   },
   data() {
     return {
@@ -70,33 +60,6 @@ export default {
   height: 100vh;
   display: flex;
   flex-wrap: wrap;
-}
-
-.header {
-  height: 8vh;
-  width: 100vw;
-  padding-left: 1vw;
-  padding-top: 1vh;
-  padding-bottom: 1vh;
-  border-bottom: black 1px solid;
-  display: flex;
-  align-items: center;
-}
-
-.logo {
-  font-size: 28px;
-  font-family: adelle, serif;
-  font-style: normal;
-  font-weight: bold;
-}
-
-.nameLabel {
-  font-family: 'Noto Sans JP', sans-serif;
-}
-
-.nameBox {
-  font-family: 'Noto Sans JP', sans-serif;
-  width: 20vw;
 }
 
 .contents {
